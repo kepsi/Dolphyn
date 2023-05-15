5144db23eb0a1385b0ca178c631d2a6cdb66e8af
Anna Lieb <anna@kepsi.eu>
2023-05-14 21:57:12 -0500

    dev advance

---
+++ dolphyn/.ipynb_checkpoints/dolphyn-checkpoint.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#import csv
+import pandas as pd
+
+def test(inputval):
+    return(inputval*10)
---
+++ dolphyn/.ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(name='dolphyn',
+version='0.1',
+description='Design package for efficient PhIP-Seq libraries',
+author='Anna Liebhoff',
+author_email='aliebho1@jhu.edu',
+license='MIT',
+py_modules=['dolphyn'],
+zip_safe=False)
