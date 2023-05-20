b6e3d00cc1acd9eba562ffbda87a00a4caee3ecb
Anna Lieb <anna@kepsi.eu>
2023-05-20 11:40:26 -0600

    script clean up

--- dolphyn/dolphyn.py
+++ dolphyn/dolphyn.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-#import csv
 import pandas as pd
+import numpy as np
 import pkg_resources
 import random
 from sklearn.ensemble import RandomForestClassifier
@@ -125,3 +125,133 @@ AA_FEAT = {'A':["sc_hydrophobic", "d_S"],
            'Y':["sc_hydrophobic", "d_Y"], 
            'V':["sc_hydrophobic", "d_I"]
           }
+
+def findEpitopes(protein_seq_file, epitile_size, testrun = 0, epitope_probability_cutoff = 0.5):
+    count = 0
+    global_epitopes = {}   
+    ep, _ = initEpiPredictor(epitile_size)    
+    
+    with open(protein_seq_file, "r") as org:
+        for line in org.readlines():
+            line = line.strip()
+            if testrun!= 0 and count/2 > testrun:
+                break
+            count = count + 1
+
+            if (count/2)%500 == 0:
+                print(str(count/2), "sequences done")
+
+            if line.startswith(">"):
+                protein = line[1:]
+            else:
+                pseq = line
+                protein_length = len(pseq)
+                epitope_proba_atpos = {}
+
+                prot_15mer_feat = kmer_features_of_protein(pseq,epitile_size)
+                probas = [class_proba[1] for class_proba in ep.predict_proba(prot_15mer_feat)]
+                for startpos in range(len(pseq)-epitile_size):
+                    epitope_proba_atpos[startpos] = np.min(probas[startpos:startpos+3])
+
+                while len(epitope_proba_atpos)>0:
+                    max_epi_proba_pos = max(epitope_proba_atpos, key=epitope_proba_atpos.get) 
+                    min_epi_proba = epitope_proba_atpos[max_epi_proba_pos]
+                    offset = np.argmax(probas[max_epi_proba_pos:max_epi_proba_pos+3])
+                    max_epi_proba_pos+=offset
+                    epi_proba = probas[max_epi_proba_pos]
+                    epi_seq = pseq[max_epi_proba_pos:max_epi_proba_pos+epitile_size]
+                    if min_epi_proba < epitope_probability_cutoff :
+                        break
+                    else:
+                        if epi_seq not in global_epitopes:
+                            global_epitopes[epi_seq] = {"probability":epi_proba,"proteins":[],"start_pos":[]}   
+                        global_epitopes[epi_seq]["proteins"].append(protein)                    
+                        global_epitopes[epi_seq]["start_pos"].append(max_epi_proba_pos)                   
+
+                        for pos in range(max_epi_proba_pos-epitile_size, max_epi_proba_pos+epitile_size):
+                            if pos in epitope_proba_atpos:
+                                del epitope_proba_atpos[pos]
+    return(global_epitopes)
+
+def saveGlobalEpitopes(global_epitopes, filename):
+    import json   
+    class NpEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, np.integer):
+                return int(obj)
+            if isinstance(obj, np.floating):
+                return float(obj)
+            if isinstance(obj, np.ndarray):
+                return obj.tolist()
+            return super(NpEncoder, self).default(obj)
+
+    with open(filename,"w") as f:
+        f.write(json.dumps(global_epitopes, cls=NpEncoder))
+        
+def readGlobalEpitopes(filename):
+    import json
+    with open(filename) as json_file:
+        global_epitopes = json.load(json_file)
+    return(global_epitopes)
+
+def peptideStitching(no_epis_per_tile, linker_seq, global_epitopes, return_unused_epis = False):
+    dolphyn_tiles = {}
+    unused_epitopes = set()
+    unused_epitopes_count = 0
+    epi_df = pd.DataFrame.from_dict(global_epitopes, orient='index')
+    epi_df["no_proteins"] = [len(v) for v in epi_df["proteins"]]
+    epi_df = epi_df.sort_values(by=["no_proteins", "probability"], ascending=False)
+    epis_to_be_treated = set(epi_df.index)
+
+    for epi in epi_df.index:
+        if epi in epis_to_be_treated:
+            #find epitopes which have the same protein set
+            pset = epi_df.loc[epi,"proteins"]
+            proteinset_epis=epi_df[epi_df["proteins"].apply(lambda x: x==pset)]
+            epis_to_be_treated = epis_to_be_treated-set(proteinset_epis.index)
+
+            #the following will drop epitopes which have lowest probability and have no partners to make tiles of 3
+            no_tiles = int(len(proteinset_epis)/no_epis_per_tile) 
+            tiles = ["" for _ in range(no_tiles)]
+            tile_probas = [list() for _ in range(no_tiles)]
+            tile_seq = ""
+            epi_index = 0
+            for stich in range(1,no_epis_per_tile+1):
+                for tn in range(no_tiles):
+                    tile_probas[tn].append(proteinset_epis.iloc[epi_index,:]["probability"])
+                    sequence = proteinset_epis.iloc[epi_index,:].name
+                    epi_index += 1
+                    if stich == 1:
+                        tiles[tn] = sequence
+                    else:
+                        tiles[tn] = tiles[tn] + linker_seq + sequence                
+
+            while len(proteinset_epis) > epi_index :
+                unused_epitopes_count += 1
+                if(return_unused_epis):
+                    unused_epitopes.add(proteinset_epis.iloc[epi_index,:].name)
+                epi_index += 1
+
+            for idx, tile in enumerate(tiles):
+                dolphyn_tiles[tile] = {"protein set":pset, 
+                                      "tile number":(idx+1), 
+                                      "tiles in protein(set)": no_tiles,
+                                     "probabilities":tile_probas[idx]}
+
+    print("total number of dolphyn tiles = ", len(dolphyn_tiles))
+    print("number of unused epitopes:", unused_epitopes_count)
+    return(dolphyn_tiles,unused_epitopes)
+
+def writeDolphynPeptidesToFAA(dolphynTiles, filename, hash_protein_names=True):
+    protein_hash = dict()
+    with open(filename, "w") as pf:
+        for seq, tile in dolphynTiles.items():
+            n = str(tile["protein set"])+"_"+str(tile["tile number"])+"of"+str(tile["tiles in protein(set)"])
+            if hash_protein_names:
+                h = abs(hash(n))
+            else:
+                h = str(n)
+            protein_hash["dolphyn_"+str(h)] = "dolphyn_"+n
+            pf.write(">"+"dolphyn_"+str(h)+"\n")
+            pf.write(seq+"*\n") # adds a stop codon in the end
+    return(protein_hash)
